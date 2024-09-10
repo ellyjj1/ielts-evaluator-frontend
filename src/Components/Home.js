ae3fd31aa98b4d84e7ac282da35fe48e8d6d2e2b
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-09-10 19:05:31 +1200

    update the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,32 +3,40 @@ import Navbar from './Navbar'
 import BannerBackground from "../Assets/home-banner-background.png";
 import BannerImage from "../Assets/home-banner-image.png";
 import { FiArrowRight } from "react-icons/fi";
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Home() {
-  return (
-    <div className='home-container'>
-        <Navbar/>
-        <div className='home-banner-container'>
-            <div className='home-bannerImage-container'>
-                <img src={BannerBackground} alt=""/>
-            </div>
-            <div className='home-text-section'>
-                <h1 className='primary-heading'>
-                Make All Your Dream Come True!
-                </h1>
-                <p className='primary-text'>
-                Achieve your dream IELTS Writing and unlock a world of opportunities, taking you wherever your heart desires!
-                </p>
-                <button className='secondary-button'>
-                Evaluate your IELTS Writing <FiArrowRight/>{""}
-                </button>
-            </div>
-            <div className='home-image-section'>
-                <img src={BannerImage} alt=''/>
+    const navigate = useNavigate();  // Get the navigate function from React Router
+
+    const handleClick = () => {
+        navigate('/chatgpt');  // Navigate to the desired route
+    };
+    return (
+        <div className='home-container'>
+            <Navbar />
+            <div className='home-banner-container'>
+                <div className='home-bannerImage-container'>
+                    <img src={BannerBackground} alt="" />
+                </div>
+                <div className='home-text-section'>
+                    <h1 className='primary-heading'>
+                        Make All Your Dream Come True!
+                    </h1>
+                    <p className='primary-text'>
+                        Achieve your dream IELTS Writing and unlock a world of opportunities, taking you wherever your heart desires!
+                    </p>
+                    <button className='secondary-button' onClick={handleClick}>
+                        Evaluate your IELTS Writing <FiArrowRight />{""}
+                    </button>
+                </div>
+                <div className='home-image-section'>
+                    <img src={BannerImage} alt='' />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
